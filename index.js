9e63f566c0d59753827a4bb69668469cfdd84250
inessouza <inessouzasp@gmail.com>
2021-09-10 21:08:26 -0300

    Projeto Guia Press

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+
+// View Engine
+app.set('view engine', 'ejs')
+
+// Static
+app.use(express.static('public'))
+
+// Body Parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Database
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso!")
+    }).catch((error) => {
+        console.log(error)
+    })
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(8080, () => {
+    console.log("Servidor Online!")
+})
