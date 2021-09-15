36f39059141041a41104e19c4bd8097b34b32a07
inessouza <inessouzasp@gmail.com>
2021-09-14 22:19:38 -0300

    Projeto GuiaPress

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,8 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const Category = require("./Category")
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
+})
+
+router.get("/categories/save", (req, res) => {
+    var title = req.body.title
+    if (title != undefined) {
+        title: title
+    } else {
+        res.redirect("/admin/categories/new")
+    }
 })
 
 module.exports = router;
