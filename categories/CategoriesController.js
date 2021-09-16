c1b685898b44d3e115bd283803c516dda04f8249
inessouza <inessouzasp@gmail.com>
2021-09-15 22:22:01 -0300

    Exclusão e Edição de Categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,18 +1,65 @@
 const express = require("express")
 const router = express.Router()
 const Category = require("./Category")
+const slugify = require("slugify")
 
 router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 })
 
-router.get("/categories/save", (req, res) => {
+router.post("/categories/save", (req, res) => {
     var title = req.body.title
     if (title != undefined) {
-        title: title
+        Category.create({
+            title: title,
+        slug: slugify(title)
+        }).then(() => {
+            res.redirect("/admin/categories")
+        })
     } else {
         res.redirect("/admin/categories/new")
     }
+})
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {categories: categories})
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+        } else {
+            res.redirect("/admin/categories")
+        }
+    } else {
+        res.redirect("/admin/categories")
+    }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if (isNaN(id)) {
+        res.redirect("/admin/categories")
+    }
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", {category: category})
+        } else {
+            res.redirect("/admin/categories")
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories")
+    })
 })
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize('guiapress', 'root', '', {
+const connection = new Sequelize('guiapress', 'root', '38013051Q!1qqqqqqq', {
     host: 'localhost',
     dialect: 'mysql'
 })
