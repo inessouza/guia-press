12ac44f5d665e088fd9140e9fd82d2238f76f488
inessouza <inessouzasp@gmail.com>
2021-09-17 19:15:20 -0300

    Edição de Artigos

--- index.js
+++ index.js
@@ -33,8 +33,14 @@ app.use("/", categoriesController)
 app.use("/", articlesController)
 
 app.get("/", (req, res) => {
-    Article.findAll().then(articles => {
-        res.render("index", {articles: articles})
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {articles: articles, categories: categories})
+        })
     })
 })
 
@@ -46,7 +52,29 @@ app.get("/:slug", (req, res) => {
         }
     }).then(article => {
         if(article != undefined) {
-            res.render("article", {article: article})
+            Category.findAll().then(categories => {
+            res.render("article", {article: article, categorie: categorie})
+        })
+        } else {
+            res.redirect("/")
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", {articles: category.articles, categories: categories})
+            })
         } else {
             res.redirect("/")
         }
