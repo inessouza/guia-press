0a9c2e5f4ed8db5261e11748faa6c77875b5a2c1
inessouza <inessouzasp@gmail.com>
2021-10-17 18:45:02 -0300

    form create

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("./User")
+const bcrypt = require('bcryptjs')
 
 router.get("/admin/users", (req, res) => {
     res.send("Listagem de Usuários")
@@ -8,6 +9,23 @@ router.get("/admin/users", (req, res) => {
 
 router.get("/admin/users/create", (req, res) => {
     res.render("admin/users/create")
+})
+
+router.post("/users/create", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect("/")
+    }).catch(() => {
+        res.redirect("/")
+    })
 })
 
 module.exports = router;
